4c1ffdd995c6094cefa2eb810f2ebbdd3f3f2ede
takeuchi-masaki <themasakitakeuchi@gmail.com>
2023-08-24 15:31:13 -0700

    align items

--- app/components/canvas.tsx
+++ app/components/canvas.tsx
@@ -2,6 +2,17 @@ import { useState, useRef, useEffect } from 'react';
 
 export default function Canvas(props: {size: number}) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas?.getContext('2d');
+        if (ctx) {
+            ctx.clearRect(0, 0, props.size, props.size);
+            ctx.fillStyle = 'red';
+            ctx.fillRect(0, 0, 100, 100);
+        }
+    });
+
     return (
         <canvas ref={canvasRef} width={props.size} height={props.size} style={{ border: '1px solid black', backgroundColor: 'white' }}></canvas>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -9,34 +9,36 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import styles from './page.module.css';
 import { useMediaQuery } from '@mui/material';
+import { blueGrey } from '@mui/material/colors';
 
 export default function Home() {
     const isMobile = useMediaQuery('(max-width: 700px)');
     return (
-        <div style={{ height: '100%' }}>
         <Box sx={{width: '98%', height: '100%'}} margin={'1%'}>
         <Stack spacing={3} sx={{height: '95%'}} padding={1} justifyContent="center">
-            <Paper sx={{backgroundColor: 'darkgrey'}} elevation={10}>
+            <Paper sx={{backgroundColor: blueGrey[500] }} elevation={10}>
                 <Typography variant="h2" margin={2} className={styles.heading}>
                     ECS 170 Optical Character Recognition Demo
                 </Typography>
             </Paper>
-            <Stack spacing={2} sx={{height: '100%'}} direction={isMobile ? 'column' : 'row'} justifyContent="center">
-                <Container sx={{backgroundColor: 'grey', height: '100%', padding: 2}}>
+            <Stack direction={isMobile ? 'column' : 'row'} spacing={2} sx={{height: '100%'}} justifyContent="center">
+                <Paper sx={{backgroundColor: blueGrey[500], height: '100%', padding: 2, width: (isMobile ? '100%' : '50%')}} elevation={5}>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
                     <Button variant='contained' sx={{margin: '10px'}}>Clear</Button>
                     <Button variant='contained' sx={{margin: '10px'}}>Undo</Button>
-                    <div></div>
-                    <Canvas size={400}></Canvas>
-                </Container>
-                <Container sx={{backgroundColor: 'grey', height: '100%', padding: 2}}>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: '10px' }}>
+                        <Canvas size={400}></Canvas>
+                    </div>
+                </Paper>
+                <Paper sx={{backgroundColor: blueGrey[500], height: '100%', padding: 2, width: (isMobile ? '100%' : '50%')}} elevation={5}>
                     <Button variant='contained' sx={{margin: '10px'}}>Predict</Button>
                     <Typography variant="h5" sx={{margin: '10px'}}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Est debitis vel soluta minima aliquam architecto aliquid voluptatem esse quibusdam excepturi repellendus culpa accusantium officia, assumenda inventore eligendi laudantium adipisci dignissimos.
                     </Typography>
-                </Container>
+                </Paper>
             </Stack>
         </Stack>
         </Box>
-        </div>
     );
 }
