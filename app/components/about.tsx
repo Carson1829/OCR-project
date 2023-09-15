d75aebdfabb45463c4ab0ca4da4569ffc5f18d12
Steven <101471432+stdChang@users.noreply.github.com>
2023-09-14 22:58:41 -0700

    Update about.tsx

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -44,29 +44,49 @@ export default function About() {
           The Model
         </Typography>
         <Typography variant="body1">
-        nitially, the scope of the project was to create an AI model trained on 
-        the MNIST dataset to create a web application that can read and predict a 
-        handwritten digit. However, we decided to broaden the scope of the project 
-        to allow for handwritten words and sentences as well. After some research, 
-        it became clear that we would be able to run a more complex model. We ultimately
-        decided on creating an AI model trained on the IAM handwriting database (https://fki.tic.heia-fr.ch/databases/iam-handwriting-database).
-        For our sentence recognition model, we had both convolutional neural network and 
-        recurrent neural network parts. The CNN component is built using residual blocks 
-        which makes training easier. This component is used to extract features from the 
-        input images, with each convolution layer focusing on certain features for the model 
-        to process. The nature of the residual blocks allows for skip connections which combine 
-        the input with the output of the convolutional layers as the total output. Residual blocks 
-        also help reduce the vanishing gradient problem. The RNN component is built using bidirectional 
-        long short term memory layers. This part is responsible for taking the output of CNN layers and 
-        processing it to predict characters or elements in the text sequence. It is able to process
-        sequences of features extracted from the images in both forward and backward directions, 
-        capturing dependencies from both directions. Dropout regularization is applied after each 
-        layer to reduce overfitting. It does this by randomly setting a fraction of inputs to 0 during 
-        training. The output of the model is the predicted text sequence. For the activation function, we 
-        used leaky relu. The difference between this and standard relu is that instead of the
-        output being 0 when x is negative, the output is a small non-zero value. 
-        This helps to prevent the dying relu problem so that neurons can still learn during
-        backpropagation even if the input was negative.
+          Our model utilizes a combination of Convolutional Neural Network (CNN)
+          layers and Recurrent Neural Network (RNN) layers. The purpose of the
+          CNN layers are to extract features of characters from the images,
+          while the RNN layers predict the sequences of characters since
+          previous characters can help predict the following characters. Our
+          model is then compiled using the CTC loss function and the Adam optimizer.
+          Initially, the scope of the project was to create an AI model trained
+          on the MNIST dataset to create a web application that can read and
+          predict a handwritten digit. However, we decided to broaden the scope
+          of the project to allow for handwritten words and sentences as well.
+          After some research, it became clear that we would be able to run a
+          more complex model. We ultimately decided on creating an AI model
+          trained on the IAM handwriting database (https://fki.tic.heia-fr.ch/databases/iam-handwriting-database).
+          For our sentence recognition model, we had both convolutional neural
+          network and recurrent neural network parts. The CNN component is built
+          using residual blocks which makes training easier. This component is
+          used to extract features from the input images, with each convolution
+          layer focusing on certain features for the model to process. The nature
+          of the residual blocks allows for skip connections which combine the
+          input with the output of the convolutional layers as the total output.
+          Residual blocks also help reduce the vanishing gradient problem.
+          The RNN component is built using bidirectional long short term memory layers.
+          This part is responsible for taking the output of CNN layers and processing
+          it to predict characters or elements in the text sequence. It is able to process
+          sequences of features extracted from the images in both forward and
+          backward directions, capturing dependencies from both directions.
+          Dropout regularization is applied after each layer to reduce overfitting.
+          It does this by randomly setting a fraction of inputs to 0 during training.
+          The output of the model is the predicted text sequence. For the activation function,
+          we used leaky relu. The difference between this and standard relu
+          is that instead of the output being 0 when x is negative, the
+          output is a small non-zero value. This helps to prevent the dying
+          relu problem so that neurons can still learn during backpropagation
+          even if the input was negative.
+          Our model was initially trained on 50 Epochs, but we decided to train
+          it again for 100 epochs to increase the accuracy of the model. The Adam
+          optimizer is used during training because it is the commonly used optimization
+          algorithm for optical character recognition tasks. It is characterized
+          by its adaptive learning rate mechanism that helps the algorithm converge
+          efficiently. The loss function is CTC (Connectionist Temporal Classification)
+          which has been noted to be good for sequence modeling problems such as text
+          and speech recognition. 
+
         </Typography>
       </Container>
       <Container>
@@ -78,7 +98,14 @@ export default function About() {
           succeeding with predicting words. The model was gradually tweaked and
           trained over different amounts of epochs. In addition to this, our
           config files were misconfigured in some cases and we updated them
-          accordingly. While connecting our backend with frontend, we dealt with
+          accordingly. We noticed that drawing large on canvas would net more
+          accurate predictions which means our canvas could be improved by normalizing
+          what is drawn on there. Certain letters remained elusive and easily confused
+          like: "j", "i", "t", "l", "s".
+          Each member had slightly different backend servers to allow 
+          them test locally as keeping the true server online costed money. Inconsistency
+          in this area was settled once more developers stuck with the Flask implementation.
+          While connecting our backend with frontend, we dealt with
           trying to convert the image to a Blob then appended to a FormData.
           After trial and error this multipart request was eventually reworked
           to be a JSON request. JSONs are more familiar to handle and we
@@ -120,8 +147,13 @@ export default function About() {
           Since none of us had significant experience with AI and machine learning before 
           taking this class, we decided to take inspiration from a tutorial we found from 
           pylessons. We learned and scaffolded code from the tutorial because we had not reached 
-          the neural network section of the class by the time we started working on the project. 
+          the neural network section of the class by the time we started working on the project.
+          <br></br> 
           The tutorial is linked here: <a href="https://pylessons.com/handwritten-sentence-recognition">https://pylessons.com/handwritten-sentence-recognition</a>
+          <br></br>
+          We used Next.js to streamline the environment for frontend. <a href="https://nextjs.org/docs">Next.js and Vercel</a>
+          <br></br>
+          Material UI library was also used in development <a href="https://mui.com/material-ui/getting-started/">Material UI</a>
         </Typography>
       </Container>
       <Container>
